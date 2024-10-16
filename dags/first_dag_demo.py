a28bd6c98c91ec308d093197971d1d69d458c006
akankshu-rajput <akankshu.kumar.c@altuspower.com>
2024-10-16 11:36:42 +0530

    Changes

--- dags/first_dag_demo.py
+++ dags/first_dag_demo.py
@@ -1,20 +1,17 @@
 from datetime import datetime
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.decorators import task
 
-with DAG(
-    dag_id="demo",
-    start_date= datetime(2024,10,16),
-    schedule=None
-):
-    hello = BashOperator(
-        task_id="hello",
-        bash_command="echo hello"
-    )
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+
+# A DAG represents a workflow, a collection of tasks
+with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
+    # Tasks are represented as operators
+    hello = BashOperator(task_id="hello", bash_command="echo hello")
 
     @task()
     def airflow():
         print("airflow")
 
-    hello >> airflow
+    # Set dependencies between tasks
+    hello >> airflow()
