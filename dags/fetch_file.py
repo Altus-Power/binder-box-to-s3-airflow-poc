459d77cd7752f8f62b3a93ac545d1c99244aab15
akankshu-rajput <akankshu.kumar.c@altuspower.com>
2024-10-16 11:30:28 +0530

    Test deg demo

--- dags/fetch_file.py
+++ dags/fetch_file.py
@@ -1,19 +1,40 @@
-from datetime import datetime
-from airflow import DAG
-from BoxtoS3Operator import BoxtoS3Operator
-
-default_args = {
-    "owner": "altus-power",
-    "depends_on_past": False,
-    "start_date": datetime(2023, 6, 1),
-    "retries": 1,
-}
-
-with DAG("box_to_s3_dag", default_args=default_args, schedule_interval=None) as dag:
-    box_to_s3_task = BoxtoS3Operator(
-        task_id="box_to_s3_transfer",
-        input_box_file_id="1672811082916",
-        output_s3_bucket="altus-document-testing",
-    )
-
-    box_to_s3_task
+# from datetime import datetime
+# from airflow import DAG
+# from BoxtoS3Operator import BoxtoS3Operator
+# from airflow.decorators import task
+#
+# default_args = {
+#     "owner": "altus-power",
+#     # "depends_on_past": False,
+#     # "start_date": datetime(2023, 6, 1),
+#     # "retries": 1,
+# }
+#
+# class Box2S3Dag:
+#     def __init__(self):
+#         self.dag_id = "box-to-s3-dag"
+#         self.dag = self._create_dag()
+#
+#     def _create_dag(self):
+#         return DAG(
+#             dag_id=self.dag_id,
+#             display_name="Box2S3",
+#             catch_up=False,
+#             start_date=datetime.now(),
+#             schedule_interval=None,
+#             default_args=default_args
+#
+#         )
+#
+#     def build_dag(self):
+#         with self.dag:
+#             @task
+#             def fetch_data(**kwargs):
+#                 pass
+#
+#
+#         return self.dag
+#
+# obj = Box2S3Dag()
+# globals()[obj.dag_id] = obj.build_dag()
+#
---
+++ dags/first_dag_demo.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id="demo",
+    start_date= datetime(2024,10,16),
+    schedule=None
+):
+    hello = BashOperator(
+        task_id="hello",
+        bash_command="echo hello"
+    )
+
+    @task
+    def airflow():
+        print("airflow")
+
+    hello >> airflow
